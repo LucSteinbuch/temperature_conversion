d3f9e4872fffef1dc70b57ac7b9a98d04743a3a1
LucB127 <luc.steinbuch@wur.nl>
2023-06-21 13:03:37 +0200

    Documentation files added

--- conversion.py
+++ conversion.py
@@ -1,4 +1,15 @@
 def celsius_to_fahrenheit(celsius):
+  """Convert degrees Celsius to Fahrenheit 
+  
+  Args: 
+    celsius (float): temperature in degrees Celsius
+    
+  Returns:  
+    float: temperature in degrees Fahrenheit
+  
+  See also https://en.wikipedia.org/wiki/Anders_Celsius and https://en.wikipedia.org/wiki/Daniel_Gabriel_Fahrenheit
+  
+  """
   return celsius/5*9+32
 
 def celsius_to_kelvin(celsius):
